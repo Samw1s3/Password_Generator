d0c4c3a158166e5b9a5b00fbcf9fff8c703eae6e
Sam Knopper <samanthaknopper@hotmail.com>
2022-03-02 21:11:10 +0800

    update javascript code, missing promit connections

--- script.js
+++ script.js
@@ -1,22 +1,102 @@
-const alpha = "abcdefghijklmnopqrstuvwxyz"
-const numbers = "0123456789"
-const symbols = "!@#$%^&*_+-="
-
-
-
-
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+const inputPasswordLowerCase = window.confirm("Include lower case letters?");
+const inputPasswordUpperCase = window.confirm("Include upper case letters?");
+const InputPasswordSpecialCharc = window.confirm("Include special characters?");
+
+const inputPasswordlength = prompt("How many characters would you like your password to be?", "type number from 8-120 here");
+
+const textareaPassword = document.getElementById("password");
+const divError = document.getElementById(div-error);
+
+const lowercaseSet = "abcdefghijklmnopqrstuvwxyz"
+const uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const symbolsSet = "!@#$%^&*_+-="
+const numbersSet = "0123456789"
+
+// When the button is clicked 
+generateBtn.addEventListener("click", function() {
+
+  const passwordLength = Number(inputPasswordLength)
+ 
+
+  let charset = "";
+
+  //const wantsLowercase =  
+ // const wantsUppercase = 
+  //const wantsSymbols = 
+  //const wantsNumbers = 
+
+
+  // in my password
+
+  if(!wantsLowercase && !wantsUppercase && !wantsSymbols && !wantsNumbers ){
+    // show alert
+    divError.textContent = "Please confirm at lease 1 selection";
+    return;
+  }
+  // Once user has confirmed selections
+
+  if(wantsLowercase){
+    charset = charset + lowercaseSet;
+  }
+  if(wantsUppercase){
+    charset = charset + uppercaseSet;
+  }
+  if(wantsSymbols){
+    charset = charset + symbolsSet;
+  }
+  if(wantsNumbers){
+    charset = charset + numbersSet;
+  }
+})
+
+//then the app will generate a password
+
+  let password = "";
+
+
+//loop for each passwordLength times,
+
+for (let index = 0; index < passwordLength; index++) {
+  const randomIndex = Math.floor(Math.random() * charset.length)
+
+  const randomChar = charset[randomIndex];
+
+  password = password + randomChar;
+}
+
+// show password on webpage
+
+textareaPassword.textContent = password;
+
+
+
+
+
+
+// When the user inputs a number for the length of that password
+
+inputpasswordLength.addEventListener( 'input', function(event){
+
+});
+
+
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+}
 
